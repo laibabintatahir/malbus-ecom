50f79d0b0ca0e615ac1a283669749456d4ddfe68
laiba <laibabintatahir@gmail.com>
2023-06-22 08:13:02 +0500

    .

--- frontend/src/Components/footer.js
+++ frontend/src/Components/footer.js
@@ -1,46 +1,60 @@
 import React from 'react';
 import '../asets/footer.css'; 
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faYoutube, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="footer-section">
-          <h3 className="footer-heading">Contact Us</h3>
-          <ul className="footer-links">
-            <li>PH # 0317-5567890</li>
-            <li>Email : fatimaaftab.1388@gmail.com</li>
-            <li>Website :www.halalwardrobe.com</li>
-        
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h3 className="footer-heading">Collection</h3>
-          <ul className="footer-links">
-           <Link to='/Components/abaya'> <li >Abaya</li></Link>
-            <Link to='/Components/hijab'><li>Hijab</li></Link>
-            <Link to='/Components/niqab'><li>Niqab</li></Link>
-            <Link to='/Components/caps'><li>Others</li></Link>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h3 className="footer-heading">Accounts</h3>
-          <div className="social-icons">
-         <a href='https://www.facebook.com/' ><FontAwesomeIcon icon={faFacebookF} /> Facebook</a>
-          <a href='https://www.instagram.com/'><FontAwesomeIcon icon={faInstagram} /> Instagram</a>
-          <a href="https://www.youtube.com/"><FontAwesomeIcon icon={faYoutube} /> Youtube</a>
-            <a href='https://web.whatsapp.com/'><FontAwesomeIcon icon={faWhatsapp} /> Whatsapp</a>
+      <div className="footer-container">
+        <div className="footer-row">
+          <div className="footer-col">
+            <h3 className="footer-heading">Luxe<span>Malbus</span></h3>
+            <p className="footer-description">
+              <strong>Malbus</strong> MALBUS derived from urdu word "malboos" the plural of libas which means cloth/clothing. Indeed we all need to cover ourselves as per the directives of Allah (SWT) but unfortunately in today's world this concept has started to vanish so identifying the gap now a days to dress with modesty gave birth to MALBUS.
+            </p>
+            <div className="footer-icons">
+              <a href="#" className="footer-icon">
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a href="#" className="footer-icon">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="#" className="footer-icon">
+                <i className="fa fa-linkedin"></i>
+              </a>
+              <a href="#" className="footer-icon">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="#" className="footer-icon">
+                <i className="fa fa-youtube"></i>
+              </a>
+            </div>
+          </div>
+          <div className="footer-col">
+            <h3 className="footer-heading">Quick Links</h3>
+            <ul className="footer-links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Message From the CEO</a></li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h3 className="footer-heading">Contact Info</h3>
+            <ul className="footer-contact">
+              <li><i className="fa fa-map-marker"></i>Islamabad, Pakistan</li>
+              <li><i className="fa fa-phone"></i>+92 3365613777</li>
+              <li><i className="fa fa-envelope"></i>
+              <a href="mailto:sagar00001.co@gmail.com" style={{color: "black"}}>malbus@gmail.com</a></li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="footer-bottom">
-                <p>&copy; HALALWARDROBE 2023</p>
-
-                <p >Powered by Fatima Aftab</p>
-            </div>
+        <p className="footer-text">
+          &copy; © 2012 - 2023 Malbus All Rights Reserved. | Designed by laiba
+        </p>
+      </div>
     </footer>
   );
 };
