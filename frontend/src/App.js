58bb750375f42514aa8cf3a5a224854a26a97726
laiba <laibabintatahir@gmail.com>
2023-06-22 06:57:16 +0500

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AppProvider } from "./components/Products/AppContext";
+import Home from "./Pages/Home";
+import Abayapage from "./Pages/Abayapage";
+import Hijabpage from "./Pages/Hijabpage";
+import Accessoriespage from "./Pages/Accessoriespage";
+import Aboutpage from "./Pages/Aboutpage";
+import Contactpage from "./Pages/Contactpage";
+import Loginpage from "./Pages/Loginpage";
+import Cartpage from "./Pages/Cartpage";
+import SignUpPage from "./Pages/SignUPage";
+import Checkout from "./Pages/Checkout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/abayapage" element={<Abayapage />} />
+          <Route path="/hijabs" element={<Hijabpage />} />
+          <Route path="/accessories" element={<Accessoriespage />} />
+          <Route path="/contactus" element={<Contactpage />} />
+          <Route path="/aboutus" element={<Aboutpage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/cart" element={<Cartpage />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        <br />
+      </div>
+    </AppProvider>
   );
-}
+};
 
 export default App;
