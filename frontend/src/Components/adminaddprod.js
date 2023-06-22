16d64304e7bc572c58342e087e68232dbfab1cad
laiba <laibabintatahir@gmail.com>
2023-06-22 21:48:06 +0500

    footer css

--- frontend/src/Components/adminaddprod.js
+++ frontend/src/Components/adminaddprod.js
@@ -1,3 +1,133 @@
+// import React from "react";
+// import Nav from "./adminnav";
+// import "../asets/admindashboard.css";
+// import { useState } from "react";
+// import { addProduct } from "../Service/api";
+
+// function AddProduct() {
+//   const [product, setProduct] = useState({
+//     name: "",
+//     category: "",
+//     price: 0,
+//     image: null,
+//     selected:false
+//   });
+
+//   const saveDetails = async (e) => {
+//     e.preventDefault();
+//     console.log("after saving : ", product);
+//     const formData=new FormData()
+
+//     formData.append("name",product.name)
+//     formData.append("category",product.category)
+//     formData.append("price",product.price)
+//     formData.append("image",product.image)
+//     console.log(formData)
+//     await addProduct(formData)
+//   };
+//   const handleChange = (e) => {
+//     setProduct({ ...product, [e.target.name]: e.target.value });
+//   };
+
+//   const handleImage=(e)=>{
+//     setProduct({...product,[e.target.name]:e.target.files[0]});
+//   }
+//   return (
+//     <div>
+//       <div>
+//         <Nav />
+//       </div>
+//       <div className="add-product" >
+//         <h3>Add New Product</h3>
+//         <div class="row" style={{ marginTop: "2%" }}>
+//           <div class="col">
+//             <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Name :</label>
+//             <input
+//               type="text"
+//               class="form-control"
+
+//               aria-label="name"
+//               name="name"
+//               onChange={handleChange}
+//             />
+//           </div>
+//         </div>
+//         <div className="row" style={{ marginTop: "2%" }}>
+//           <div class="col">
+//           <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Price :</label>
+//             <input
+//               type="number"
+//               class="form-control"
+//               aria-label="Last name"
+//               name="price"
+//               onChange={handleChange}
+//             />
+//           </div>
+//         </div>
+//         <div class="row" style={{ marginTop: "2%" }}>
+//           <div class="col">
+//           <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Category :</label>
+//           <select className="form-control">
+//             <option>
+//               abaya
+//             </option>
+//             <option>
+//               hijab
+//             </option>
+//             <option>
+//               niqab
+//             </option>
+//             <option>
+//               bundle
+//             </option>
+//             <option>
+//               cap
+//             </option>
+//           </select>
+//             <input
+//               type="text"
+//               class="form-control"
+//               placeholder="Categorie"
+//               aria-label="First name"
+//               name="category"
+//               onChange={handleChange}
+//             />
+//           </div>
+//         </div>
+//         <div class="row" style={{ marginTop: "2%" }}>
+//           <div class="col">
+//           <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Name :</label>
+//             <input
+//               type="file"
+//               class="form-control"
+//               aria-label="First name"
+//               name="image"
+//               onChange={handleImage}
+//             />
+//           </div>
+//         </div>
+//         <div className="row">
+//           <button
+//             style={{
+//               marginTop: "2%",
+//               width: "40%",
+//               marginLeft: "30%",
+//               background: "pink",
+//               borderRadius: "10px",
+//             }}
+//             onClick={saveDetails}
+//           >
+//             Save
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default AddProduct;
+
+
 import React from "react";
 import Nav from "./adminnav";
 import "../asets/admindashboard.css";
@@ -10,42 +140,44 @@ function AddProduct() {
     category: "",
     price: 0,
     image: null,
-    selected:false
+    selected: false
   });
 
   const saveDetails = async (e) => {
     e.preventDefault();
-    console.log("after saving : ", product);
-    const formData=new FormData()
+    console.log("after saving:", product);
+    const formData = new FormData();
 
-    formData.append("name",product.name)
-    formData.append("category",product.category)
-    formData.append("price",product.price)
-    formData.append("image",product.image)
-    console.log(formData)
-    await addProduct(formData)
+    formData.append("name", product.name);
+    formData.append("category", product.category);
+    formData.append("price", product.price);
+    formData.append("image", product.image);
+    console.log(formData);
+    await addProduct(formData);
   };
+
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
 
-  const handleImage=(e)=>{
-    setProduct({...product,[e.target.name]:e.target.files[0]});
-  }
+  const handleImage = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.files[0] });
+  };
+
   return (
     <div>
       <div>
         <Nav />
+        <br />   <br />
       </div>
-      <div className="add-product" >
-        <h3>Add New Product</h3>
-        <div class="row" style={{ marginTop: "2%" }}>
-          <div class="col">
-            <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Name :</label>
+      <div className="add-product">
+        <h3>Add Product</h3>
+        <div className="row" style={{ marginTop: "2%" }}>
+          <div className="col">
+            <label style={{ marginTop: '2%', marginBottom: '1%' }}>Product Name:</label>
             <input
               type="text"
-              class="form-control"
-              
+              className="form-control"
               aria-label="name"
               name="name"
               onChange={handleChange}
@@ -53,53 +185,42 @@ function AddProduct() {
           </div>
         </div>
         <div className="row" style={{ marginTop: "2%" }}>
-          <div class="col">
-          <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Price :</label>
+          <div className="col">
+            <label style={{ marginTop: '2%', marginBottom: '1%' }}>Product Price:</label>
             <input
               type="number"
-              class="form-control"
+              className="form-control"
               aria-label="Last name"
               name="price"
               onChange={handleChange}
             />
           </div>
         </div>
-        <div class="row" style={{ marginTop: "2%" }}>
-          <div class="col">
-          <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Category :</label>
-          <select className="form-control">
-            <option>
-              abaya
-            </option>
-            <option>
-              hijab
-            </option>
-            <option>
-              niqab
-            </option>
-            <option>
-              bundle
-            </option>
-            <option>
-              cap
-            </option>
-          </select>
+        <div className="row" style={{ marginTop: "2%" }}>
+          <div className="col">
+            <label style={{ marginTop: '2%', marginBottom: '1%' }}>Product Category:</label>
+            <select className="form-control">
+              <option>abaya</option>
+              <option>hijab</option>
+              <option>niqab</option>
+              <option>cap</option>
+            </select>
             <input
               type="text"
-              class="form-control"
-              placeholder="Categorie"
+              className="form-control"
+              placeholder="Category"
               aria-label="First name"
               name="category"
               onChange={handleChange}
             />
           </div>
         </div>
-        <div class="row" style={{ marginTop: "2%" }}>
-          <div class="col">
-          <label style={{marginTop:'2%',marginBottom:'1%'}}>Product Name :</label>
+        <div className="row" style={{ marginTop: "2%" }}>
+          <div className="col">
+            <label style={{ marginTop: '2%', marginBottom: '1%' }}>Product Image:</label>
             <input
               type="file"
-              class="form-control"
+              className="form-control"
               aria-label="First name"
               name="image"
               onChange={handleImage}
--- frontend/src/Components/admindashboard.js
+++ frontend/src/Components/admindashboard.js
@@ -1,18 +1,40 @@
+// import React from 'react';
+// import Nav from './adminnav';
+
+// const AdminDashboard = () => {
+//   return (
+//     <div>
+//         <div>
+//             <Nav />
+//          </div>
+//          <div style={{width:'100vw'}}>
+//             <img src='../images/admin.jpg' alt='aly' style={{marginLeft:'16%',marginTop:'-5%'}}/>
+//          </div>
+//     </div>
+    
+//   );
+// };
+
+// export default AdminDashboard;
+
 import React from 'react';
 import Nav from './adminnav';
 
 const AdminDashboard = () => {
   return (
     <div>
-        <div>
-            <Nav />
-         </div>
-         <div style={{width:'100vw'}}>
-            <img src='../images/admin.jpg' alt='aly' style={{marginLeft:'16%',marginTop:'-5%'}}/>
-         </div>
+      <div>
+        <Nav />
+      </div>
+      <div style={{ position: 'relative' }}>
+        <img src='../images/admin.jpg' alt='aly' style={{ width: '100%', height: '700px' }} />
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+          <h1 style={{ color: 'white', fontSize: '3rem', textShadow: '2px 2px 4px rgba(0, 0, 0, 1.0)' }}>Welcome to the admin page</h1>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
 export default AdminDashboard;
+
