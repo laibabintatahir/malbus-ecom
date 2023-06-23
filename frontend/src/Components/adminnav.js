1b5395c68b389d72801bdd01601156de5483aaca
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-06-23 05:00:56 +0500

    Something idk

--- frontend/src/Components/adminnav.js
+++ frontend/src/Components/adminnav.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import '../asets/admindashboard.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
-  const navigate=useNavigate();
-  const handleHome=()=>{
+  const navigate = useNavigate();
+  const handleHome = () => {
     navigate('/Components/home');
-  }
-  const handleAdd=()=>{
-    navigate('/Components/adminaddprod');
-  }
-  const handlecustomer=()=>{
+  };
+  const handleAdd = () => {
+    navigate('/adminaddprod');
+  };
+  const handlecustomer = () => {
     navigate('/Components/admincustomer');
-  }
+  };
   return (
-    <div className="navbar">
-      <div className="logo2-container">
-        <img src='../images/Logo.jpg' alt="Logo" className="logo2" />
+    <div className='navbar'>
+      <div className='logo2-container'>
+        <img src='../images/Logo.jpg' alt='Logo' className='logo2' />
       </div>
-      <div className="menu-container">
-        <ul className="menu">
-          <li className="menu-item" onClick={handleHome}>Home</li>
-          <li className="menu-item" onClick={handleAdd}>Add Product</li>
-          <li className="menu-item" onClick={handlecustomer}>View Customers</li>
+      <div className='menu-container'>
+        <ul className='menu'>
+          <li className='menu-item' onClick={handleHome}>
+            Home
+          </li>
+          <li className='menu-item' onClick={handleAdd}>
+            Add Product
+          </li>
+          <li className='menu-item' onClick={handlecustomer}>
+            View Customers
+          </li>
         </ul>
       </div>
     </div>
